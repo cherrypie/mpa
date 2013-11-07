82fa2fdfaf7c5552153df74d0a2241bd24d586f6
Vladyslav Kryukov <cherrypie@users.noreply.github.com>
2013-11-07 19:52:51 +0200

    Update LongNumber.h
    I hope it still can be compiled :/

--- LongNumber.h
+++ LongNumber.h
@@ -1,62 +1,52 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <sstream>
-#include <stdlib.h>
-#include <time.h>
-#include <limits.h>
 #include <utility>
 
 #define BITSINWORD 32
 
-using namespace std;
-
-unsigned long long ext_gcd(unsigned long long a, unsigned long long b, long long& x, long long& y);
-unsigned long long inverse(unsigned long long a, unsigned long long n);
-
 class LongNumber
 {
-private:
-	vector <unsigned int> arr;
+protected:
+	std::vector <unsigned int> arr;
 
 	bool nonNegative;
 
 	LongNumber& add(LongNumber&);
 	LongNumber& sub(LongNumber&);
-	pair<LongNumber, unsigned int> divByte(unsigned int&);
-	static pair<LongNumber, LongNumber> div(LongNumber, LongNumber&);
+	std::pair<LongNumber, unsigned int> divByte(unsigned int&);
+	static std::pair<LongNumber, LongNumber> div(LongNumber, LongNumber&);
 	static unsigned int const c1 = 0x7fffffff;
 	static unsigned long long const Base = 1ULL << sizeof(int) * 8;
 
 public:
 
-	//Base
+	//Input/Output
 	LongNumber();
 	LongNumber(unsigned int);
-	LongNumber(string);
+	LongNumber(std::string);
 	LongNumber(const LongNumber&);	
-	friend ostream& operator<<(ostream&, const LongNumber&);
+	friend ostream& operator<<(std::ostream&, const LongNumber&);
 
 
-	//Logic
-	friend bool operator<(const LongNumber&, const LongNumber&);
+	//Comparison operators
+	friend bool operator<(LongNumber&, LongNumber&);
 	friend bool operator==(LongNumber&, LongNumber&);
-	friend bool operator==(const LongNumber&, unsigned int);
 
 
-	//Arithmetic
+	//Arithmetic operations
 	friend LongNumber operator+(LongNumber&, LongNumber&);
 	friend LongNumber operator+(LongNumber&, unsigned int);
 	friend LongNumber operator-(LongNumber&, LongNumber&);
 	friend LongNumber operator*(LongNumber&, LongNumber&);
-	friend LongNumber operator*(LongNumber, unsigned int);
+	friend LongNumber operator*(LongNumber&, unsigned int); //what a hack was here in prev version?
 	friend LongNumber operator/(LongNumber&, LongNumber&);
 	friend LongNumber operator/(LongNumber&, unsigned int);
 	friend LongNumber operator%(LongNumber&, LongNumber&);
 	LongNumber Square();
 
 
-	//Modular
+	//Modular operations
 	static LongNumber Barrett(LongNumber, LongNumber&, LongNumber&);
 	static LongNumber MonRed(LongNumber, LongNumber&);
 	static LongNumber MonPro(LongNumber&, LongNumber&, LongNumber&, unsigned int c = 0);
@@ -64,7 +54,7 @@ public:
 	static LongNumber MonOddPow(LongNumber&, LongNumber&, LongNumber&);
 	static LongNumber Pow(LongNumber&, LongNumber&, LongNumber&);
 
-	//Other
+	//Other stuff
 	unsigned int getSize(){
 		return arr.size();
 	}
