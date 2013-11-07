6771fa03e527129aaabdac763036bf5705191c12
Vladyslav Kryukov <cherrypie@users.noreply.github.com>
2013-11-07 19:55:38 +0200

    Update LongNumber_Base.cpp

--- LongNumber_Base.cpp
+++ LongNumber_Base.cpp
@@ -17,11 +17,11 @@ LongNumber::LongNumber(const LongNumber& data)
 	arr.assign(data.arr.begin(), data.arr.end());
 }
 
-LongNumber::LongNumber(string str)
+LongNumber::LongNumber(std::string str)
 {
 	nonNegative = 1;
-	string buff;
-	string::iterator itr = str.end(), end = str.begin();
+	std::string buff;
+	std::string::iterator itr = str.end(), end = str.begin();
 	for(; itr - end >= sizeof(int) * 2; itr -= sizeof(int) * 2)
 		arr.push_back(stoul(buff.assign(itr - sizeof(int) * 2, itr), nullptr, 16));
 	if(itr > end)
@@ -29,10 +29,10 @@ LongNumber::LongNumber(string str)
 	while(arr.size() > 1 && arr[arr.size() - 1] == 0) arr.pop_back();
 }
 
-ostream& operator<<(ostream& out, const LongNumber& data)
+ostream& operator<<(std::ostream& out, const LongNumber& data)
 {
 	if(!data.nonNegative) out << '-';
-	out << hex << data.arr[data.arr.size() - 1];
+	out << std::hex << data.arr[data.arr.size() - 1];
 	for(int i = data.arr.size() - 2; i >= 0; --i)
 		for(int j = sizeof(int) * 2 - 1; j >= 0; --j)
 			out << ((data.arr[i] >> j * 4) & 0xf);
